64deb08a10829190a770fd7e0edaa5e7b512b769
Fréderic LOUI <frederic.loui@renater.fr>
2024-04-09 22:42:42 +0200

    Added proto folder

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
-        .compile(&["../holo/proto/holo.proto"], &["../holo/proto"])?;
+        .compile(&["./proto/holo.proto"], &["./proto"])?;
     Ok(())
 }
